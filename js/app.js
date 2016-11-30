62c225ef8c4485832a0df6688e11436fc77a4387
Albane Beltzung <albane.beltzung@hotmail.com>
2016-11-30 11:19:11 +0100

    getHint function added: when pressing button you get another hint

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Raptor on 13/06/16.
  */
-(function() {
+(function () {
     var aires = angular.module('aires', [
         'ngRoute',
         'airesControllers',
@@ -20,60 +20,51 @@
     });
 
 
-    aires.config(['$routeProvider', function($routeProvider){
+    aires.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/', {
-            templateUrl: 'templates/inicio.html',
-            controller: 'InicioController'
-        })
+                templateUrl: 'templates/inicio.html',
+                controller: 'InicioController'
+            })
 
             .when("/trainings", {
-             templateUrl : "templates/trainings.html"
-        })
+                templateUrl: "templates/trainings.html"
+            })
 
             .when("/content-mastery", {
-                templateUrl : "templates/content-mastery.html"
+                templateUrl: "templates/content-mastery.html"
             })
 
             .otherwise('/', {
-            templateUrl: 'templates/inicio.html'
-        })
+                templateUrl: 'templates/inicio.html'
+            })
     }]);
 })();
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
 
-function openGame(target){
-    document.getElementById(target).style.display = "block"
-}
-function closeGame(target){
-    document.getElementById(target).style.display = "none"
-}
 
 function togGame(target) {
     if (document.getElementById(target).style.display == "none") {
-        document.getElementById(target).style.display = "block"
+        showHint();
+        document.getElementById(target).style.display = "block";
     } else {
-        document.getElementById(target).style.display = "none"
+        document.getElementById(target).style.display = "none";
     }
+}
+
+var count = 0;
+function showHint() {
+    var hints = document.getElementsByClassName("hint");
+    newHint = hints[count];
+    newHint.style.display = "block";
+    count = count+1;
 }
