10573c0678d8e4689edc628ced587f2c7affce6d
Daniel Risso Elliot <daniel@rissell.me>
2016-12-03 12:28:44 -0600

    Angular Funcions

--- js/app.js
+++ js/app.js
@@ -2,78 +2,31 @@
  * Created by Raptor on 13/06/16.
  */
 (function() {
-    var aires = angular.module('aires', [
+    var cite = angular.module('cite', [
         'ngRoute',
-        'airesControllers',
+        'citeControllers',
         'ngSanitize'
     ]);
 
-    aires.value('header', {
-        brand: 'todos',
-        model: 'todos',
-        type: 'todos'
-    });
-
-    aires.value('comparar', {
-        items: [0, 0, 0],
-        numero: 0
-    });
-
-
-    aires.config(['$routeProvider', function($routeProvider){
+    cite.config(['$routeProvider', function($routeProvider){
         $routeProvider
             .when('/', {
             templateUrl: 'templates/inicio.html',
-            controller: 'InicioController'
+            controller: 'contentMasteryController'
         })
 
             .when("/trainings", {
-             templateUrl : "templates/trainings.html"
+                templateUrl : "templates/trainings.html",
+                controller: "contentMasteryController"
         })
 
-            .when("/content-mastery", {
-                templateUrl : "templates/content-mastery.html"
+            .when("/content-mastery/:game", {
+                templateUrl : "templates/content-mastery.html",
+                controller: "contentMasteryController"
             })
 
             .otherwise('/', {
-            templateUrl: 'templates/inicio.html'
+                templateUrl: 'templates/inicio.html'
         })
     }]);
 })();
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-
-function openGame(target){
-    document.getElementById(target).style.display = "block"
-}
-function closeGame(target){
-    document.getElementById(target).style.display = "none"
-}
-
-function togGame(target) {
-    if (document.getElementById(target).style.display == "none") {
-        document.getElementById(target).style.display = "block"
-    } else {
-        document.getElementById(target).style.display = "none"
-    }
-}
--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,29 @@
 /**
  * Created by Raptor on 13/06/16.
  */
-var airesControllers = angular.module('airesControllers', []);
-airesControllers.controller('InicioController', ['$scope',  'header', function($scope, header){
+var citeControllers = angular.module('citeControllers', []);
+citeControllers.controller('headerController', ['$scope', function($scope){
+    /* Set the width of the side navigation to 250px */
+    $scope.openNav = function() {
+        document.getElementById("mySidenav").style.width = "250px";
+    };
+
+    /* Set the width of the side navigation to 0 */
+    $scope.closeNav = function() {
+        document.getElementById("mySidenav").style.width = "0";
+    };
+
+
+}]);
+
+citeControllers.controller('contentMasteryController', ['$scope', '$routeParams', function($scope, $routeParams){
+
+    $scope.game = $routeParams.game;
+
+    $scope.toGame = function(gameNum){
+        $scope.game1 = true;
+        $scope.hint = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore";
+    }
+
 
 }]);
